c132ad606f4dba9bae98ce7aa852692ece813379
Piotr_Pietraszek <pan.pietraszek@gmail.com>
2020-02-08 23:30:37 +0100

    Updates

---
+++ precache-manifest.87276c0aa58cd6a53b39f166e866ea9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9037fb9090c3cbf3a70cb978163254b",
+    "url": "/climb-form/index.html"
+  },
+  {
+    "revision": "cd6ad9b0a6975cbd79bc",
+    "url": "/climb-form/static/css/main.f50c69e8.chunk.css"
+  },
+  {
+    "revision": "f12e62f2f0fcf6b46b4e",
+    "url": "/climb-form/static/js/2.6e979893.chunk.js"
+  },
+  {
+    "revision": "c58823e8cd690e54df2e7452d0f1a139",
+    "url": "/climb-form/static/js/2.6e979893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd6ad9b0a6975cbd79bc",
+    "url": "/climb-form/static/js/main.7d671d0b.chunk.js"
+  },
+  {
+    "revision": "758475ead0990a5874ad",
+    "url": "/climb-form/static/js/runtime-main.63cdd0a7.js"
+  },
+  {
+    "revision": "beca89d5da25286a87d167884f2cd5e6",
+    "url": "/climb-form/static/media/bgImg-2.beca89d5.jpg"
+  },
+  {
+    "revision": "0073b9e01fd199ebe4203e15afd1844d",
+    "url": "/climb-form/static/media/climber.0073b9e0.png"
+  }
+]);
